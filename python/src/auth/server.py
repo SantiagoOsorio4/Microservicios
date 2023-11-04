4bbd856a39b3a0e1c5c454caff3c6019712e4ad9
santiagoOsorio <santiagoosoriocordoba@gmail.com>
2023-11-04 03:50:51 -0500

    vim init.sql

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -5,8 +5,17 @@ from flask_mysqldb import MySQL
 server = Flask(__name__)
 mysql = MySQL(server)
 
-# config
+
 
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-print(server.config["MYSQL_HOST"])
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.enviton.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
+@server.route("/login", methods=["POST"])
+def login():
+     auth = request.authorization
+     if not auth:
+         return "missing credentials", 401
+     
